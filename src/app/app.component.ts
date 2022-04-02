a214f46a2cc62bab37189f2410ea5025bf3dca18
Yashendra Gupta <yashendragupta2704@gmail.com>
2022-04-02 14:51:18 +0530

    added code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { App } from './3d/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fun-with-3d';
+  
+  ngAfterViewInit(): void {
+    const app3d = new App('renderCanvas');
+    app3d.run();
+  }
 }
